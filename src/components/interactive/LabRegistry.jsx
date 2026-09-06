617bdc594e12bf898293dca16806a6c1b3d391e4
David Williams <davidvwilliams@yahoo.com>
2026-09-06 04:03:48 -0400

    Update LabRegistry.jsx

--- src/components/interactive/LabRegistry.jsx
+++ src/components/interactive/LabRegistry.jsx
@@ -1,18 +1,15 @@
-// Version: v1.0 - Centralized Interactive Lab Registry
-// Maps curriculum module IDs to their respective interactive micro-labs.
-
 import DOMOrderBookSimulator from './DOMOrderBookSimulator.jsx';
 import DualEngineArchitectureDiagram from './DualEngineArchitectureDiagram.jsx';
 import BarLifecycleScrubber from './BarLifecycleScrubber.jsx';
 import BodyRangeRatioLab from './BodyRangeRatioLab.jsx';
+import InsideBarCompressionLab from './InsideBarCompressionLab.jsx'; // 1. Import new lab
 
 const LAB_REGISTRY = {
   'tier0-mod-0.1': DOMOrderBookSimulator,
   'tier0-mod-0.2': DualEngineArchitectureDiagram,
   'tier1-mod-1.1': BarLifecycleScrubber,
   'tier1-mod-1.2': BodyRangeRatioLab,
-  // Future components will be registered here in 1 line:
-  // 'tier1-mod-1.3': InsideBarCompressionLab,
+  'tier1-mod-1.3': InsideBarCompressionLab, // 2. Register module ID
 };
 
 export function getInteractiveLab(moduleId) {
