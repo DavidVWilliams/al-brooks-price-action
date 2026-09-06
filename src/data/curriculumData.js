a738312f71f45b1f71c249d7936530577781c4d6
David Williams <davidvwilliams@yahoo.com>
2026-09-06 02:42:03 -0400

    Update curriculumData.js

--- src/data/curriculumData.js
+++ src/data/curriculumData.js
@@ -5,14 +5,18 @@ import tier0 from './tiers/tier0.json';
 import tier1 from './tiers/tier1.json';
 import tier2 from './tiers/tier2.json';
 import tier3 from './tiers/tier3.json';
+import tier4 from './tiers/tier4.json';
+import tier5 from './tiers/tier5.json';
 
 export const curriculumData = {
-  version: 'v4.0-modular-tiers',
+  version: 'v5.0-master-complete',
   modules: [
     ...tier0.modules,
     ...tier1.modules,
     ...tier2.modules,
-    ...tier3.modules
+    ...tier3.modules,
+    ...tier4.modules,
+    ...tier5.modules
   ]
 };
 
